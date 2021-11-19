631212d8151947082b5112d2d404a6d2b003a083
Harpunen Harry <harald.tributsch@outlook.com>
2021-11-19 14:31:45 +0100

    Added sample project

---
+++ app/src/main/java/at/tributsch/msp_android_opengl_es/GlEs30JniActivity.java
@@ -0,0 +1,25 @@
+package at.tributsch.msp_android_opengl_es;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GlEs30JniActivity extends Activity {
+
+    GlEs30JniView mView;
+
+    @Override protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        mView = new GlEs30JniView(getApplication());
+        setContentView(mView);
+    }
+
+    @Override protected void onPause() {
+        super.onPause();
+        mView.onPause();
+    }
+
+    @Override protected void onResume() {
+        super.onResume();
+        mView.onResume();
+    }
+}
